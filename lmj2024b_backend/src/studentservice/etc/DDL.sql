ee3264c7c6fff5d12b2a68dac688efc772f7ff33
leeminjin98 <lmj98202@gmail.com>
2025-01-12 18:42:47 +0900

    2개월 최종과제 end

--- lmj2024b_backend/src/studentservice/etc/DDL.sql
+++ lmj2024b_backend/src/studentservice/etc/DDL.sql
@@ -1,3 +1,15 @@
 use studentservice;
 # 점수등록	insert into 테이블명( 속성명 , 속성명 ) values( 속성값 , 속성값 );
-insert into student( sname , kscore , escore , mscore ) values( '값' , '값' , '값' , '값' );
+insert into student( sname , kscore , escore , mscore ) values( '값' , 0 , 0 , 0 );
+
+# 점수조회
+select * from student;
+
+# 점수수정
+update student set sname = 'sname' , kscore = 0 , escore = 0 , mscore = 0 where sno = 2;
+
+# 점수수정 조회
+select * from student where sno = 12;
+
+# 점수 삭제
+delete from student where sno = 1;re , escore , mscore ) values( '값' , '값' , '값' , '값' );
